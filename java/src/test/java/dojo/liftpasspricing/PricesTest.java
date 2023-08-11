677536be28ac19f3e7f4e0859fbb0caedce3c7aa
Michael R. Wolf <MichaelRWolf@att.net>
2023-08-11 15:04:11 -0400

    e   Convert to lambda (final problem)

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -3,9 +3,6 @@ package dojo.liftpasspricing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PricesTest {
