5478e2cde867af725396fe2d045cca2ddcea639a
Michael R. Wolf <MichaelRWolf@att.net>
2023-08-11 10:47:47 -0400

    t   New test to exercise getPricesHandler

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -4,12 +4,45 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PricesTest {
+
+
+    @Test
+    public void testGetPricesHandler() throws SQLException, ParseException {
+        // given
+        CostForTypeProvider costForTypeProvider = new CostForTypeProvider() {
+            @Override
+            public int getCostForLiftTicketType(String liftTicketType) throws SQLException {
+                return 17;
+            }
+
+            @Override
+            public void setLiftPassCostForLiftPassType(int liftPassCost, String liftPassType) throws SQLException {
+            }
+        };
+        HolidaysProvider holidaysProvider = new HolidaysProvider() {
+            @Override
+            public List<Date> getHolidays() throws SQLException {
+                return null;
+            }
+        };
+        Prices prices = new Prices();
+        // when
+        // then
+        assertEquals("{ \"cost\": 17}",
+                prices.getPricesHandler(costForTypeProvider,
+                        holidaysProvider,
+                        "42",
+                        "night",
+                        "2023-08-10"));
+    }
+
 
     @BeforeAll
     public static void createPrices() throws SQLException {
@@ -29,16 +62,6 @@ public class PricesTest {
                 return null;
             }
         };
-    }
-
-    @Test
-    public void doesSomething() {
-        assertEquals(2+2, 4);
-    }
-
-    @Test
-    public void doPutGetHandlerBusinessLogic() {
-        // write some tests for the business logic
     }
 
 }
