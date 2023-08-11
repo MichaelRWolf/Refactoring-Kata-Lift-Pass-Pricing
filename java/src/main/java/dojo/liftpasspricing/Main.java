3aa9f9c204546747a7e86a8fc73c067adddb658e
Michael R. Wolf <MichaelRWolf@att.net>
2023-08-11 09:39:07 -0400

    Revert "r  Extract methods for put/get (to aid testing)"
    This reverts commit 01eb41dc73e04422a28c573311008023e0f34219.

--- java/src/main/java/dojo/liftpasspricing/Main.java
+++ java/src/main/java/dojo/liftpasspricing/Main.java
@@ -11,10 +11,14 @@ public class Main {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
         DatabaseUtilities databaseUtilities = new DatabaseUtilities();
+        CostForTypeProvider costForTypeProvider = databaseUtilities;
+        HolidaysProvider holidaysProvider = databaseUtilities;
+
         new Prices().createApplication(costForTypeProvider, holidaysProvider);
+        DatabaseUtilities dbu = databaseUtilities;
 
         @SuppressWarnings("resource")
-        Connection connection = databaseUtilities.getConnection();
+        Connection connection = dbu.getConnection();
 
         System.out.println("LiftPassPricing Api started on 4567,\n"
                 + "you can open http://localhost:4567/prices?type=night&age=23&date=2019-02-18 in a navigator\n"
