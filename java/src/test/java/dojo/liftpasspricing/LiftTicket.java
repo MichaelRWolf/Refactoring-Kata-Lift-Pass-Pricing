461bb61731ab35858e951161407287f50d50bec4
Mike Kienenberger <mkienenb@gmail.com>
2023-08-18 11:29:46 -0400

    t create date with /holiday when Christmas

--- java/src/test/java/dojo/liftpasspricing/LiftTicket.java
+++ java/src/test/java/dojo/liftpasspricing/LiftTicket.java
@@ -1,5 +1,6 @@
 package dojo.liftpasspricing;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class LiftTicket {
@@ -10,8 +11,22 @@ public class LiftTicket {
     @Override
     public String toString() {
         return "{liftTicketType: " + liftTicketType + ", " +
-                "usageDate: " + usageDate + ", " +
+                // Decorate date with /holiday if it's a holiday
+                "usageDate: " + decorated(usageDate) +
+                ", " +
                 "skierAge: " + skierAge + "}";
+    }
+
+    private String decorated(Date usageDate) {
+        return this.usageDate.toString()
+                + (isHoliday() ?  "/holiday" : "");
+    }
+
+    private boolean isHoliday() {
+        Calendar usageDateCalendar = Calendar.getInstance();
+        usageDateCalendar.setTime(usageDate);
+        return usageDateCalendar.get(Calendar.MONTH) == Calendar.DECEMBER
+                && usageDateCalendar.get(Calendar.DAY_OF_MONTH) == 25;
     }
 
     public LiftTicket(String liftTicketType, Date usageDate, int skierAge) {
